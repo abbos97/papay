acfee9d074dbc3a061a4ab16f76eb0f314d8f8e0
abbos97 <yourali97@gmail.com>
2023-01-24 09:12:17 +0900

    K-task

---
+++ train.js
@@ -0,0 +1,9 @@
+// ============= K-task ==================
+
+
+function footBallPoints(w,d,l) {
+    let wins = 3,draws = 1,losses = 0;
+     return w + d + l + wins + draws + losses
+}
+
+console.log(footBallPoints(3,4,2))
