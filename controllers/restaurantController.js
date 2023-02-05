221ad11ae311a669a47c62dcf07d7c50d636380c
abbos97 <yourali97@gmail.com>
2023-02-06 02:18:42 +0900

    feat: restaurant user getting restaurant related products

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,98 +1,94 @@
-const Member = require("../models/Member")
+const Member = require("../models/Member");
+const Product = require("../models/Product");
 let restaurantController = module.exports;
 
-restaurantController.getMyRestaurantData = async (req, res) => {
-    try {
-        console.log("GET: cont/getMyRestaurantData")
-        // TODO: Get my restaurant products
+restaurantController.getMyRestaurantProducts = async (req, res) => {
+  try {
+    console.log("GET: cont/getMyRestaurantProducts");
+    const product = new Product();
+    const data = await product.getAllProductsDataResto(res.locals.member);
 
-        res.render('restaurant-menu')
-    } catch (err) {
-        console.log(`ERROR: cont/getMyRestaurantData ${err.message}`)
-        res.json({state: "fail", message: err.message})
-    }
-}
-
+    res.render("restaurant-menu", {restaurant_data: data});
+  } catch (err) {
+    console.log(`ERROR: cont/getMyRestaurantProducts ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
+};
 
 restaurantController.getSignupMyRestaurant = async (req, res) => {
-    try {
-        console.log("GET: cont/getSignupMyRestaurant")
-        res.render("signup")
-    } catch (err) {
-        console.log(`ERROR: cont/getSignupMyRestaurant ${err.message}`)
-        res.json({state: "fail", message: err.message})
-    }
-}
-
+  try {
+    console.log("GET: cont/getSignupMyRestaurant");
+    res.render("signup");
+  } catch (err) {
+    console.log(`ERROR: cont/getSignupMyRestaurant ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
+};
 
 restaurantController.signupProcess = async (req, res) => {
-    try {
-        console.log(`POST: cont/signup`)
-        const data = req.body,
-          member = new Member(),
-          new_member = await member.signupData(data);
+  try {
+    console.log(`POST: cont/signup`);
+    const data = req.body,
+      member = new Member(),
+      new_member = await member.signupData(data);
 
-          // SESSION
+    // SESSION
 
-          req.session.member = new_member;
-          res.redirect('/resto/products/menu')
-
-    }catch(err) {
-        console.log(`ERROR: cont/signup ${err.message}`)
-        res.json({state: "fail", message: err.message})
-    }
-}
-
+    req.session.member = new_member;
+    res.redirect("/resto/products/menu");
+  } catch (err) {
+    console.log(`ERROR: cont/signup ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
+};
 
 restaurantController.getLoginMyRestaurant = async (req, res) => {
-    try {
-        console.log("GET: cont/getLoginMyRestaurant")
-        res.render("login-page")
-    } catch (err) {
-        console.log(`ERROR: cont/getLoginMyRestaurant ${err.message}`)
-        res.json({state: "fail", message: err.message})
-    }
-}
-
-
+  try {
+    console.log("GET: cont/getLoginMyRestaurant");
+    res.render("login-page");
+  } catch (err) {
+    console.log(`ERROR: cont/getLoginMyRestaurant ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
+};
 
 restaurantController.loginProcess = async (req, res) => {
-    try {
-        console.log(`POST: cont/login`)
-        const data = req.body,
-          member = new Member(),
-          result = await member.loginData(data);
+  try {
+    console.log(`POST: cont/login`);
+    const data = req.body,
+      member = new Member(),
+      result = await member.loginData(data);
 
-        req.session.member = result;
-        req.session.save(function() {
-            res.redirect('/resto/products/menu')
-        });
-
-
-    }catch(err) {
-        console.log(`ERROR: cont/login ${err.message}`)
-        res.json({state: "fail", message: err.message})
-    }
-}
-
-
+    req.session.member = result;
+    req.session.save(function () {
+      res.redirect("/resto/products/menu");
+    });
+  } catch (err) {
+    console.log(`ERROR: cont/login ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
+};
 
 restaurantController.logout = (req, res) => {
-    console.log("GET cont.logout")
-    res.send("logout sahifasidasiz")
-}
+  console.log("GET cont.logout");
+  res.send("logout sahifasidasiz");
+};
 
 restaurantController.validateAuthRestaurant = (req, res, next) => {
-    if(req.session?.member?.mb_type === "RESTAURANT") {
-        req.member = req.session.member;
-        next()
-    } else res.json({state: "fail", message: "only authenticated member with restaurant type"})
-}
+  if (req.session?.member?.mb_type === "RESTAURANT") {
+    req.member = req.session.member;
+    next();
+  } else
+    res.json({
+      state: "fail",
+      message: "only authenticated member with restaurant type",
+    });
+};
 
 restaurantController.checkSessions = (req, res) => {
-    if(req.session?.member) {
-        res.json({state: 'succeed', data: req.session.member})
-    } else {
-        res.json({state: 'fail', message: 'you are not authenticated'})
-    }
-}
+  if (req.session?.member) {
+    res.json({ state: "succeed", data: req.session.member });
+  } else {
+    res.json({ state: "fail", message: "you are not authenticated" });
+  }
+};
--- router_bssr.js
+++ router_bssr.js
@@ -1,36 +1,33 @@
 const express = require("express");
 const router_bssr = express.Router();
-const restaurantController = require("./controllers/restaurantController")
-const productController = require("./controllers/productController")
-const upload_product = require("./utils/upload-multer")("products")
+const restaurantController = require("./controllers/restaurantController");
+const productController = require("./controllers/productController");
+const upload_product = require("./utils/upload-multer")("products");
 /**************************************
  *             BSSR EJS               *
  **************************************/
 
- 
-
-
-
 // memberga dahildor routerlar
 router_bssr
-.get("/signup", restaurantController.getSignupMyRestaurant)
-.post("/signup", restaurantController.signupProcess)
+  .get("/signup", restaurantController.getSignupMyRestaurant)
+  .post("/signup", restaurantController.signupProcess);
 router_bssr
-.get("/login", restaurantController.getLoginMyRestaurant)
-.post("/login", restaurantController.loginProcess)
-router_bssr.get("/logout", restaurantController.logout)
-router_bssr.get("/check-me", restaurantController.checkSessions)
+  .get("/login", restaurantController.getLoginMyRestaurant)
+  .post("/login", restaurantController.loginProcess);
+router_bssr.get("/logout", restaurantController.logout);
+router_bssr.get("/check-me", restaurantController.checkSessions);
 
-
-router_bssr.get("/products/menu", restaurantController.getMyRestaurantData)
-router_bssr.post("/products/create", 
- restaurantController.validateAuthRestaurant,
- upload_product.array('product_images', 5),
- productController.addNewProduct)
-router_bssr.post("/products/edit/:id",
-restaurantController.validateAuthRestaurant,
-productController.updateChosenProduct)
-
-
+router_bssr.get("/products/menu", restaurantController.getMyRestaurantProducts);
+router_bssr.post(
+  "/products/create",
+  restaurantController.validateAuthRestaurant,
+  upload_product.array("product_images", 5),
+  productController.addNewProduct
+);
+router_bssr.post(
+  "/products/edit/:id",
+  restaurantController.validateAuthRestaurant,
+  productController.updateChosenProduct
+);
 
 module.exports = router_bssr;
