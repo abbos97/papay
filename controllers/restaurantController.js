3555dc9f59a58f112321e929fae64c322cd4a13f
abbos97 <yourali97@gmail.com>
2023-02-11 09:26:50 +0900

    fix: develop business logic of signup page on adminka

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,5 +1,10 @@
+const assert = require("assert");
 const Member = require("../models/Member");
 const Product = require("../models/Product");
+const Definer = require("../lib/mistake");
+
+
+
 let restaurantController = module.exports;
 
 
@@ -39,14 +44,20 @@ restaurantController.getSignupMyRestaurant = async (req, res) => {
 
 restaurantController.signupProcess = async (req, res) => {
   try {
-    
-    const data = req.body,
-      member = new Member(),
-      new_member = await member.signupData(data);
+    console.log("GET: cont/signupProcess");
+    assert(req.file, Definer.general_err3);
 
-    // SESSION
 
-    req.session.member = new_member;
+    const new_member = req.body;
+    new_member.mb_type = 'RESTAURANT';
+    new_member.mb_image = req.file.path;
+
+
+    const  member = new Member(),
+           result = await member.signupData(new_member);
+
+    assert(result, Definer.general_err1);
+    req.session.member = result;
     res.redirect("/resto/products/menu");
   } catch (err) {
     console.log(`ERROR: cont/signupProcess ${err.message}`);
--- router_bssr.js
+++ router_bssr.js
@@ -3,6 +3,8 @@ const router_bssr = express.Router();
 const restaurantController = require("./controllers/restaurantController");
 const productController = require("./controllers/productController");
 const upload_product = require("./utils/upload-multer")("products");
+const upload_members = require("./utils/upload-multer")("members");
+
 /**************************************
  *             BSSR EJS               *
  **************************************/
@@ -13,7 +15,11 @@ router_bssr.get("/", restaurantController.home);
 
 router_bssr
   .get("/sign-up", restaurantController.getSignupMyRestaurant)
-  .post("/sign-up", restaurantController.signupProcess);
+  .post(
+    "/sign-up",
+    upload_members.single("restaurant-img"),
+    restaurantController.signupProcess
+  );
 router_bssr
   .get("/login", restaurantController.getLoginMyRestaurant)
   .post("/login", restaurantController.loginProcess);
